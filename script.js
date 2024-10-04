2d5ad239dcb45e686eee15783b80297cc6f3dc66
Adenilson Alves <140216618+eng-adenilson@users.noreply.github.com>
2024-10-04 00:36:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,21 @@
 document.getElementById("checkButton").addEventListener("click", () => {
     const nameInput = document.getElementById("nameInput").value.trim();
-<<<<<<< HEAD
     const resultElement = document.getElementById("resultado");
     const imageElement = document.getElementById("image");
 
 
-=======
-    const resultElement = document.getElementById("result");
-    const imageElement = document.getElementById("image");
-
->>>>>>> 2a560830bbed848445f629875478c977c43d7f6b
     // Gerando um resultado aleatório
     const isCorno = Math.random() < 0.5;
 
     if (isCorno) {
         resultElement.textContent = `${nameInput}, você é corno!`;
         resultElement.style.color = "red";
-<<<<<<< HEAD
         imageElement.src = "corno.jpg"; 
-=======
-        imageElement.src = "image.png"; 
->>>>>>> 2a560830bbed848445f629875478c977c43d7f6b
         imageElement.style.display = "block"; 
     } else {
         resultElement.textContent = `${nameInput}, você não é corno!`;
         resultElement.style.color = "green";
-<<<<<<< HEAD
         imageElement.style.display = "none"; 
     }
 
 });
-=======
-        imageElement.style.display = "none"; 
-    }
-});
->>>>>>> 2a560830bbed848445f629875478c977c43d7f6b
