2a560830bbed848445f629875478c977c43d7f6b
Luan Ferreira <luanhsouzaf@gmail.com>
2024-10-04 00:16:33 -0300

    subindo

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("checkButton").addEventListener("click", () => {
+    const nameInput = document.getElementById("nameInput").value.trim();
+    const resultElement = document.getElementById("result");
+    const imageElement = document.getElementById("image");
+
+    // Gerando um resultado aleatório
+    const isCorno = Math.random() < 0.5;
+
+    if (isCorno) {
+        resultElement.textContent = `${nameInput}, você é corno!`;
+        resultElement.style.color = "red";
+        imageElement.src = "image.png"; 
+        imageElement.style.display = "block"; 
+    } else {
+        resultElement.textContent = `${nameInput}, você não é corno!`;
+        resultElement.style.color = "green";
+        imageElement.style.display = "none"; 
+    }
+});
